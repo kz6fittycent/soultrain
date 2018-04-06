ebb9b584c520c0c243df27db30ebec8b82666075
kz6fittycent <jimmy.tigert@gmail.com>
2018-04-06 17:30:45 -0500

    Update __init__.py

--- libsoultrain/__init__.py
+++ libsoultrain/__init__.py
@@ -3,6 +3,7 @@
 from os import path
 import random
 import textwrap
+import tkinter as tk
 
 module_data = path.join(path.dirname(__file__), "data")
 ascii_path = path.join(module_data, "ascii")
@@ -10,3 +11,4 @@ ascii_path = path.join(module_data, "ascii")
 def main():
     with open(ascii_path) as ascii_file:
          print(ascii_file.read))
+    
