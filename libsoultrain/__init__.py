a4669a2d4a49f17feb0f91f69bc61454220da7cd
kz6fittycent <jimmy.tigert@gmail.com>
2018-04-06 16:58:46 -0500

    Create __init__.py

---
+++ libsoultrain/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from os import path
+import random
+import textwrap
+
+module_data = path.join(path.dirname(__file__), "data")
+ascii_path = path.join(module_data, "ascii")
+
+def main():
+    with open(ascii_path) as ascii_file:
+         print(ascii_file.read))
