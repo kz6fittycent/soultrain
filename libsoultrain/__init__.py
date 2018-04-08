2b78e06d5c5ed0d6045d79270553298ad8174cdb
kz6fittycent <jimmy.tigert@gmail.com>
2018-04-07 19:06:33 -0500

    Update __init__.py

--- libsoultrain/__init__.py
+++ libsoultrain/__init__.py
@@ -12,6 +12,7 @@ def main():
     with open(ascii_path) as ascii_file:
          print(ascii_file.read)
          print(ascii_file.closed)
+    with open(ascii2_path) as ascii2_file:    
          print(ascii2_file.read)
          print(ascii2_file.closed)
     
