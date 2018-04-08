e023eecb871859a39eeca3a80357dc91b98c354e
kz6fittycent <jimmy.tigert@gmail.com>
2018-04-07 19:21:24 -0500

    Update __init__.py

--- libsoultrain/__init__.py
+++ libsoultrain/__init__.py
@@ -11,7 +11,7 @@ ascii2_path = path.join(module_data, "ascii2")
 def main():
     with open(ascii_path) as ascii_file:
          print(ascii_file.read)
-         print(ascii_file.closed)
+         print(ascii_file.closed))
    # with open(ascii2_path) as ascii2_file:    
         # print(ascii2_file.read)
         # print(ascii2_file.closed)
